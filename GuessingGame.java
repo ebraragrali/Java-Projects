d4ee3aacecda6bef7a22eefd5b8fd9d558dc511d
Ebrar Ağralı <157977459+ebraragrali@users.noreply.github.com>
2024-10-11 22:15:03 +0300

    Update GuessingGame.java

--- GuessingGame.java
+++ GuessingGame.java
@@ -1,32 +1,32 @@
 public class GuessingGame {
-  public static void main(String[] args) {
-      int randomNum = (int)(Math.random() * 101);  
-      int tahmin = 0;
-      int denemeSayisi = 0;
-      
-      System.out.println("1 ile 100 arasında bir sayı tuttum. Tahmin etmeye çalış");
-      System.out.println("Oyunu durdurmak için 'q' yazın.");
+    public void startGame() {
+        int randomNum = (int)(Math.random() * 101);  
+        int tahmin = 0;
+        int denemeSayisi = 0;
+        
+        System.out.println("1 - 100 arasında bir sayı tuttum, tahmin etmeye çalış");
+        System.out.println("Oyunu durdurmak için 'q' yaz.");
 
-      while (tahmin != randomNum) {
-          System.out.print("Tahmininizi girin: ");
-          String input = System.console().readLine();  
+        while (tahmin != randomNum) {
+            System.out.print("Tahmininizi girin: ");
+            String input = System.console().readLine();  
 
-          if (input.equalsIgnoreCase("q")) {
-              System.out.println("Oyunu sonlandırdınız.");
-              break;
-          }
+            if (input.equalsIgnoreCase("q")) {
+                System.out.println("Oyunu sonlandırdınız.");
+                break;
+            }
 
-          tahmin = Integer.parseInt(input);
-          denemeSayisi++;
-          
-          if (tahmin < randomNum) {
-              System.out.println("Daha büyük bir sayı girin.");
-          } else if (tahmin > randomNum) {
-              System.out.println("Daha küçük bir sayı girin.");
-          } else {
-              System.out.println("Tebrikler! Doğru tahmin.");
-              System.out.println("Toplam deneme sayısı: " + denemeSayisi);
-          }
-      }
-  }
+            tahmin = Integer.parseInt(input);
+            denemeSayisi++;
+            
+            if (tahmin < randomNum) {
+                System.out.println("Daha büyük bir sayı girin.");
+            } else if (tahmin > randomNum) {
+                System.out.println("Daha küçük bir sayı girin.");
+            } else {
+                System.out.println("Tebriklerrrr! Doğru tahmin.");
+                System.out.println("Toplam deneme sayısı: " + denemeSayisi);
+            }
+        }
+    }
 }
